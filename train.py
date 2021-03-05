8c602c60860eee8a725c02aa4074e4781f4d6974
David Wöhlert <david.woehlert@gmx.de>
2021-03-05 15:40:45 +0100

    Feature contribution

--- train.py
+++ train.py
@@ -48,9 +48,10 @@ print(train.groupby('Neighborhood', as_index=False)['YearBuilt'].mean())
 data = [train,test]
 numeric_list = ['Alley','Street','PoolQC','MiscFeature','Pool','2ndFlr']
 encode_list = ['GarageQual','GarageCond','GarageFinish','GarageType','Heating','HeatingQC','CentralAir','ExterCond','ExterQual','MSZoning','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinType2','Foundation','PavedDrive','Functional','Electrical','SaleType','SaleCondition','Fence','FireplaceQu','KitchenQual','LotConfig','LandSlope','Neighborhood','LandContour','LotShape','Condition1','Condition2','BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType']
-drop_list_columns = ['Utilities','PoolArea','FireplaceQu','MasVnrType','TotRmsAbvGrd','Condition2','LandContour','Id','MiscVal','YrSold','LowQualFinSF','BsmtFinType1','BsmtFinType2','PoolQC','Street','MoSold','LandSlope','Exterior2nd','MSSubClass','Foundation','SaleType','LotConfig','2ndBsmtFlr','1stFlrSF','2ndFlrSF']
-quantile_list = ['SalePrice','LotArea','LotFrontage','YearBuilt','GrLivArea','TotalBsmtSF','GrLivArea','MasVnrArea','BsmtUnfSF','BsmtFinSF1','BsmtFinSF2','1stFlrSF','2ndFlrSF','GarageArea','WoodDeckSF','PorchSF']
+drop_list_columns = ['Utilities','PoolArea','FireplaceQu','MasVnrType','TotRmsAbvGrd','Condition2','LandContour','Id','MiscVal','YrSold','LowQualFinSF','BsmtFinType1','BsmtFinType2','PoolQC','Street','MoSold','LandSlope','Exterior2nd','Foundation','SaleType','LotConfig','2ndBsmtFlr','1stFlrSF','2ndFlrSF']
+quantile_list = ['SalePrice','LotArea','LotFrontage','YearBuilt','GrLivArea','TotalBsmtSF','GrLivArea','MasVnrArea','BsmtUnfSF','BsmtFinSF1','BsmtFinSF2','1stFlrSF','2ndFlrSF','GarageArea','WoodDeckSF','PorchSF','QualitySum','OverallQual']
 garage_list = ['GarageType','GarageYrBlt','GarageFinish','GarageQual','GarageCond','GarageArea']
+quality_sum_list =['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual','GarageCond','PoolQC']
 bath_list = ['BsmtFullBath','BsmtHalfBath','FullBath','HalfBath']
 bmst_list = ['BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinType2','BsmtUnfSF','BsmtFinSF1']
 porch_list = ['OpenPorchSF','ScreenPorch','EnclosedPorch','3SsnPorch']
@@ -135,6 +136,18 @@ for i in data:
 			i.at[k, '2ndBsmtFlr'] = 1
 		else:
 			i.at[k, '2ndBsmtFlr'] = 0
+		i.at[k, 'QualitySum'] = 0
+		for l in quality_sum_list:
+			if i.at[k, l] == 'Ex':
+				i.at[k, 'QualitySum'] += 5
+			elif i.at[k, l] == 'Gd':
+				i.at[k, 'QualitySum'] += 4
+			elif i.at[k, l] == 'TA':
+				i.at[k, 'QualitySum'] += 3
+			elif i.at[k, l] == 'Fa':
+				i.at[k, 'QualitySum'] += 2
+			elif i.at[k, l] == 'Po':
+				i.at[k, 'QualitySum'] += 1				
 		##i.at[k, 'BuildSF'] = i.at[k, '1stFlrSFSF'] + i.at[k, '2ndFlrSF']
 		i.at[k, 'PorchSF'] = i.at[k, 'OpenPorchSF'] + i.at[k, 'EnclosedPorch'] + i.at[k, '3SsnPorch'] + i.at[k, 'ScreenPorch']
 		i.at[k, 'Bath_count'] = i.at[k, 'BsmtFullBath'] + i.at[k, 'FullBath'] + 0.5*i.at[k, 'BsmtHalfBath'] + 0.5*i.at[k, 'HalfBath']
@@ -213,7 +226,7 @@ for i in data:
 
 ###remove columns with low correlation to price
 for col in train.columns:
-	if math.sqrt((train['SalePrice'].corr(train[col]))**2) < 0.2:
+	if math.sqrt((train['SalePrice'].corr(train[col]))**2) < 0.1:
 		train.drop(col, axis=1, inplace=True)
 		test.drop(col, axis=1, inplace=True)
 
@@ -282,7 +295,10 @@ xg_reg.fit(x_train,y_train)
 Y_pred = xg_reg.predict(x_test)
 ##print("Accuracy XGBoost (RMSLE):",np.sqrt(metrics.mean_squared_log_error(y_test, Y_pred)))
 print("Accuracy XGBoost (RMSLE):",np.sqrt(metrics.mean_squared_log_error(np.expm1(y_test), np.expm1(Y_pred))))
-
+f,ax = plt.subplots(figsize=(10,10))
+xgb.plot_importance(xg_reg,ax=ax,color='red')
+plt.savefig('graphs/feature_contribution_xgboost.png')
+plt.close
 ####Optimized XGBoost
 # param_grid = {
 #      'colsample_bytree': np.linspace(0.3, 1, 4),
